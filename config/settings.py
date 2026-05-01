1efe57f077d062262565c844efa4315fc52bda5c
jecko sayo <jeckosayo16@gmail.com>
2026-05-01 11:52:57 +0800

    remove sqlite and add gitignore

--- config/settings.py
+++ config/settings.py
@@ -58,19 +58,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default="sqlite:///db.sqlite3",
-#         conn_max_age=600
-#     )
-# }
-
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600
     )
 }
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#     )
+# }
 
 
 LANGUAGE_CODE = 'en-us'
